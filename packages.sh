2e64cdc7891e786577adf8f62e24ec0a477c0ef8
Amit Manchanda <amitdelhi1995@gmail.com>
2016-11-30 10:49:15 +0530

    Added shell script to install all packages required by me

---
+++ packages.sh
@@ -0,0 +1,8 @@
+apt install -y vim chromium-browser gedit python-pip python3-pip terminator artha okular git
+pip3 install --upgrade pip
+pip install --upgrade pip
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+pip3 install theano
+pip install theano
+
